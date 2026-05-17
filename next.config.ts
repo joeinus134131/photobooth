6a8f6a1da467cc9f46c33d132a08fd1230554d44
Made Agus Andi Gunawan <49284230+joeinus134131@users.noreply.github.com>
2026-05-17 20:24:36 +0700

    chore: make Sentry webpack wrapper strictly conditional on token presence

--- next.config.ts
+++ next.config.ts
@@ -5,14 +5,17 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withSentryConfig(nextConfig, {
-  org: process.env.SENTRY_ORG,
-  project: process.env.SENTRY_PROJECT,
-  authToken: process.env.SENTRY_AUTH_TOKEN,
-  silent: true, // Suppress Sentry CLI debug messages
-  widenClientFileUpload: true,
-  // Disable source map uploads if SENTRY_AUTH_TOKEN is missing to avoid Vercel build crashes
-  sourcemaps: {
-    disable: !process.env.SENTRY_AUTH_TOKEN,
-  },
-});
+const sentryEnabled =
+  !!process.env.SENTRY_AUTH_TOKEN &&
+  !!process.env.SENTRY_ORG &&
+  !!process.env.SENTRY_PROJECT;
+
+export default sentryEnabled
+  ? withSentryConfig(nextConfig, {
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      silent: true, // Suppress Sentry CLI debug messages
+      widenClientFileUpload: true,
+    })
+  : nextConfig;
